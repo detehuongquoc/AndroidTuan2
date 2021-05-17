dbc142fc304d859ead0f82889f4137b918dd0598
duc <3beonhi3@gmail.com>
2021-05-17 18:00:35 +0700

    done

---
+++ app/src/main/java/com/example/a18077431_nguyenphanhuynhduc_android_tuan2/Activity_Bai1.java
@@ -0,0 +1,14 @@
+package com.example.a18077431_nguyenphanhuynhduc_android_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Bai1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__bai1);
+    }
+}
---
+++ app/src/main/java/com/example/a18077431_nguyenphanhuynhduc_android_tuan2/activity_bai2.java
@@ -0,0 +1,14 @@
+package com.example.a18077431_nguyenphanhuynhduc_android_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_bai2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2);
+    }
+}
